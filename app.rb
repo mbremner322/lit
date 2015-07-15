3a80396cf67ffed4e7221d72009510289c725953
Marie <mbremner@andrew.cmu.edu>
2015-07-15 10:10:15 +0900

    first commit in new directory

--- app.rb
+++ app.rb
@@ -1,27 +1,52 @@
-require 'bundler/setup'
-Bundler.require
-require 'sinatra/reloader' if development?
-require 'open-uri'
-require "sinatra/json"
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+require 'heroku'
+require 'sinatra/base'
+require 'pry-byebug'
+enable :sessions
+class SillyCatz < Sinatra::Base
+    TITLE= "Slides For Scrubs"
+    
+    
+    def get_file(name)
+        name = '/episodes/'+name
+    end
 
-Dotenv.load
+    
+    get '/' do
+        erb :home
+    end
 
-require './image_uploader.rb'
-require './models.rb'
 
-get '/' do
-  @contents = Contribution.order('id desc').all
-  erb :index
-end
 
-post '/new' do
-  logger.info "名前：#{params[:user_name]}、内容：#{params[:body]}"
-  Contribution.create(:name => params[:user_name], :body => params[:body], :img => "")
-  
-  if params[:file]
-    image_upload(params[:file])
-  end
-  
-  
-  redirect '/'
-end
+    get '/courseSelection' do
+        erb :selectCourse
+    end
+    
+    get '/webDesign' do
+        erb :sampleCourse
+    end
+    
+    get '/dnd-grade' do
+        
+        @result = params[:correctness].split(',')
+        @result.collect! { |each|
+            each = each.split(':')
+        }
+        @numOfQ = @result.length
+        @numCorrect = 0
+        @result.each do |each|
+            if each[1] == 'true'
+                @numCorrect += 1
+            end
+        end
+        erb :graded        
+    end
+end 
+
+run SillyCatz.run!
+
+
+
+
